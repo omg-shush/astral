12dbe3b5dab62bc2e9b3597cc654f03913e85a59
omg-shush <possiblychell@gmail.com>
2023-10-14 04:10:57 -0700

    Add perlin noise generator and plane-based terrain generation

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,146 @@
-use bevy::{prelude::*, input::mouse::MouseMotion, app::AppExit, window::CursorGrabMode};
-use terrain_plane::TerrainPlanePlugin;
+use std::f32::consts::PI;
+use rand::random;
 
-use crate::terrain_plane::TerrainPlane;
+use bevy::{prelude::*, input::mouse::MouseMotion, app::AppExit, window::CursorGrabMode};
+use bevy_framepace::{FramepacePlugin, FramepaceSettings, Limiter};
+
+use crate::terrain_plane::{TerrainPlane, TerrainPlanePlugin};
 
 mod terrain_plane;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, TerrainPlanePlugin::default()))
+        .insert_resource(ClearColor(Color::rgba(0.7, 0.7, 1.0, 1.0)))
+        .add_plugins((DefaultPlugins, TerrainPlanePlugin::default(), FramepacePlugin))
         .add_systems(Startup, startup)
         .add_systems(Update, (update_move, update_look, exit_game))
         .run();
 }
 
-fn startup(mut commands: Commands, mut materials: ResMut<Assets<StandardMaterial>>, meshes: ResMut<Assets<Mesh>>, mut window: Query<&mut Window>) {
+fn startup(mut commands: Commands, mut materials: ResMut<Assets<StandardMaterial>>, mut meshes: ResMut<Assets<Mesh>>, mut window: Query<&mut Window>, mut frames: ResMut<FramepaceSettings>) {
     println!("Hello, world!");
 
     let mut window = window.single_mut();
     window.cursor.grab_mode = CursorGrabMode::Locked;
     window.cursor.visible = false;
 
-    let material = materials.add(StandardMaterial {
-        base_color: Color::CYAN,
+    frames.limiter = Limiter::from_framerate(60.);
+
+    // Terrain
+    let material_terrain = materials.add(StandardMaterial {
+        base_color: Color::WHITE,
+        reflectance: 0.1,
         ..default()
     });
-
-    let plane = TerrainPlane::new(meshes);
-    let mesh = plane.mesh.clone();
-
-    commands.spawn((plane, MaterialMeshBundle {
-        mesh,
-        material,
+    let perlin_1 = perlin(100);
+    let perlin_2 = perlin(100);
+    let perlin_3 = perlin(100);
+    let perlin_4 = perlin(100);
+    let terrain_heightmap = |x: f32, y: f32| {
+        [
+            perlin_1(x / 3., y / 3.),
+            perlin_2(x / 13., y / 13.) * 4.,
+            perlin_3(x / 43., y / 43.) * 16.,
+            perlin_4(x / 197., y / 197.) * 64.
+        ].iter().sum()
+    };
+    let terrain = TerrainPlane::new(&mut meshes, terrain_heightmap);
+    let terrain_handle = terrain.mesh.clone();
+    commands.spawn((terrain, MaterialMeshBundle {
+        mesh: terrain_handle,
+        material: material_terrain,
         ..default()
     }));
 
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 9000.,
-            range: 100.,
-            shadows_enabled: true,
+    // Rocks
+    let material_rocks = materials.add(StandardMaterial {
+        base_color: Color::DARK_GRAY,
+        reflectance: 0.001,
+        ..default()
+    });
+    let perlin_1 = perlin(100);
+    let perlin_2 = perlin(100);
+    let perlin_3 = perlin(100);
+    let perlin_4 = perlin(100);
+    let rocks_heightmap = |x: f32, y: f32| {
+        let t = terrain_heightmap(x, y);
+        if t < 8. || t > 26. {
+            return -32.;
+        }
+        [
+            t,
+            perlin_1(x / 3., y / 3.) * 1.,
+            perlin_2(x / 13., y / 13.) * 2.,
+            perlin_3(x / 23., y / 23.) * 4.,
+            perlin_4(x / 197., y / 197.) * 0.5,
+            -2.
+        ].iter().sum()
+    };
+    let rocks = TerrainPlane::new(&mut meshes, rocks_heightmap);
+    let rocks_handle = rocks.mesh.clone();
+    commands.spawn((rocks, MaterialMeshBundle {
+        mesh: rocks_handle,
+        material: material_rocks,
+        ..default()
+    }));
+
+    // Water
+    let material_water = materials.add(StandardMaterial {
+        base_color: Color::rgba(0.2, 0.2, 0.9, 0.45),
+        reflectance: 0.4,
+        alpha_mode: AlphaMode::Blend,
+        ..default()
+    });
+    let perlin_1 = perlin(100);
+    let perlin_2 = perlin(100);
+    let heightmap = |x: f32, y: f32| {
+        [
+            perlin_1(x / 1.5, y / 1.5) * 0.3,
+            perlin_2(x / 7., y / 7.) * 0.8,
+        ].iter().sum()
+    };
+    let water = TerrainPlane::new(&mut meshes, heightmap);
+    let water2 = TerrainPlane::new(&mut meshes, |x, y| heightmap(x + 34., y - 12.));
+    let water3 = TerrainPlane::new(&mut meshes, |x, y| heightmap(x + 11., y + 64.));
+    let water4 = TerrainPlane::new(&mut meshes, |x, y| heightmap(x - 22., y - 36.));
+    commands.spawn(MaterialMeshBundle {
+        mesh: water.mesh.clone(),
+        material: material_water.clone(),
+        transform: Transform::from_translation(-16. * Vec3::Y),
+        ..default()
+    });
+    commands.spawn(MaterialMeshBundle {
+        mesh: water2.mesh.clone(),
+        material: material_water.clone(),
+        transform: Transform::from_translation(-17. * Vec3::Y),
+        ..default()
+    });
+    commands.spawn(MaterialMeshBundle {
+        mesh: water3.mesh.clone(),
+        material: material_water.clone(),
+        transform: Transform::from_translation(-18. * Vec3::Y),
+        ..default()
+    });
+    commands.spawn(MaterialMeshBundle {
+        mesh: water4.mesh.clone(),
+        material: material_water.clone(),
+        transform: Transform::from_translation(-19. * Vec3::Y),
+        ..default()
+    });
+
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            color: Color::WHITE,
+            illuminance: 8000.,
+            shadows_enabled: false,
             ..default()
         },
-        transform: Transform::from_xyz(8., 16., 8.),
+        transform: Transform::from_translation(Vec3::ZERO).looking_at(Vec3::new(-5., -3., -8.), Vec3::Y),
         ..default()
     });
 
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0., 6., 0.).looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
+        transform: Transform::from_xyz(0., 64., 12.).looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
         ..default()
     });
 }
@@ -55,17 +149,18 @@ fn update_move(mut camera: Query<&mut Transform, With<Camera>>, keys: Res<Input<
     let mut camera = camera.single_mut();
     let forward = camera.forward();
     let right = camera.right();
+    let speed = if keys.pressed(KeyCode::ShiftLeft) { 100. } else { 10. };
     if keys.pressed(KeyCode::W) {
-        camera.translation += forward * 10. * time.delta_seconds();
+        camera.translation += forward * speed * time.delta_seconds();
     }
     if keys.pressed(KeyCode::S) {
-        camera.translation -= forward * 10. * time.delta_seconds();
+        camera.translation -= forward * speed * time.delta_seconds();
     }
     if keys.pressed(KeyCode::A) {
-        camera.translation -= right * 10. * time.delta_seconds();
+        camera.translation -= right * speed * time.delta_seconds();
     }
     if keys.pressed(KeyCode::D) {
-        camera.translation += right * 10. * time.delta_seconds();
+        camera.translation += right * speed * time.delta_seconds();
     }
 }
 
@@ -84,5 +179,32 @@ fn update_look(mut camera: Query<&mut Transform, With<Camera>>, mut mouse: Event
 fn exit_game(keys: Res<Input<KeyCode>>, mut exit: EventWriter<AppExit>) {
     if keys.pressed(KeyCode::Escape) {
         exit.send(AppExit::default());
+    }
+}
+
+fn perlin(size: usize) -> impl Fn(f32, f32) -> f32 {
+    let mut map = vec![vec![Vec2::ZERO; size]; size];
+    for xi in 0..size {
+        for yi in 0..size {
+            map[xi][yi] = Vec2::from_angle(2. * PI * random::<f32>());
+        }
+    }
+    move |x, y| {
+        let (xi, yi) = (x.rem_euclid(size as f32), y.rem_euclid(size as f32));
+        let (xi_f, yi_f) = (xi.floor(), yi.floor());
+        let points = vec![
+            Vec2::new(xi_f, yi_f),
+            Vec2::new(xi_f, yi_f+1.),
+            Vec2::new(xi_f+1., yi_f),
+            Vec2::new(xi_f+1., yi_f+1.),
+        ];
+        let gradients = points.iter().map(|p| map[p.x as usize % size][p.y as usize % size]); // % size in case floating point error
+        let offsets = points.iter().map(|p| Vec2::new(xi, yi) - *p);
+        let dot_offsets = gradients.zip(offsets).map(|(g, o)| (g.dot(o), o)).collect::<Vec<_>>();
+        let smoothstep = |x: f32| 6. * x.powi(5) - 15. * x.powi(4) + 10. * x.powi(3);
+        let interp = |a, b, p| a + (b - a) * smoothstep(p);
+        let interp_x = |(d1, p1): (f32, Vec2), (d2, _)| (interp(d1, d2, p1.x), Vec2::new(0., p1.y));
+        let interp_y = |(d1, p1): (f32, Vec2), (d2, _)| interp(d1, d2, p1.y);
+        interp_y(interp_x(dot_offsets[0b00], dot_offsets[0b10]), interp_x(dot_offsets[0b01], dot_offsets[0b11]))
     }
 }
--- src/terrain_plane.rs
+++ src/terrain_plane.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, render::{render_resource::PrimitiveTopology, mesh::Indices}, core::Zeroable};
+use bevy::{prelude::*, render::{render_resource::PrimitiveTopology, mesh::Indices}};
 
 #[derive(Default)]
 pub struct TerrainPlanePlugin {}
@@ -13,24 +13,24 @@ pub struct TerrainPlane {
 }
 
 impl TerrainPlane {
-    pub fn new(mut meshes: ResMut<Assets<Mesh>>) -> TerrainPlane {
+    pub fn new(meshes: &mut ResMut<Assets<Mesh>>, heightmap: impl Fn(f32, f32) -> f32) -> TerrainPlane {
         let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
 
-        let (width, height) = (100u16, 100u16);
+        let (width, height) = (1000u32, 1000u32);
         let unit = 1.;
+
+        let idx = |x, y| x * (height + 1) + y;
 
         let mut positions = vec![Vec3::ZERO; ((width+1) * (height+1)) as usize];
         let mut normals = vec![Vec3::ZERO; ((width+1) * (height+1)) as usize];
         let mut indices = Vec::with_capacity((width * height * 6) as usize);
-
-        let idx = |x, y| x * (height + 1) + y;
 
         // Compute vertex positions & indices
         for xi in 0..=width {
             for yi in 0..=height {
                 let (x, y) = ((xi as f32 - (width as f32/2.)) * unit, (yi as f32 - (height as f32/2.)) * unit);
                 let attr_idx = idx(xi, yi) as usize;
-                positions[attr_idx] = Vec3::new(x, (x*x+y).sin() / 4., y);
+                positions[attr_idx] = Vec3::new(x, heightmap(x, y), y);
                 normals[attr_idx] = Vec3::Y; // TODO recompute
                 if xi > 0 && yi > 0 {
                     // Draw quad between (xi-1, yi-1) and (xi, yi)
@@ -56,15 +56,57 @@ impl TerrainPlane {
         for i in 0..normals.len() {
             normals[i] = normals[i].normalize();
         }
+        // Gaussian smooth
+        let mut new_normals = vec![Vec3::ZERO; ((width+1) * (height+1)) as usize];
+        for xi in 0..=width as i32 {
+            for yi in 0..=height as i32 {
+                let gaussian_kernel = [
+                    (xi,   yi,   4.),
+                    (xi,   yi-1, 2.),
+                    (xi,   yi+1, 2.),
+                    (xi-1, yi,   2.),
+                    (xi-1, yi-1, 1.),
+                    (xi-1, yi+1, 1.),
+                    (xi+1, yi,   2.),
+                    (xi+1, yi-1, 1.),
+                    (xi+1, yi+1, 1.),
+                ];
+                new_normals[idx(xi as u32, yi as u32) as usize] = gaussian_kernel.into_iter()
+                    .fold(Vec3::ZERO, |normal_acc, (x, y, weight)| {
+                        let x = x.clamp(0, width as i32);
+                        let y = y.clamp(0, height as i32);
+                        normal_acc + weight * normals[idx(x as u32, y as u32) as usize]
+                    }
+                ).normalize();
+            }
+        }
+        normals = new_normals;
 
         assert!(positions.len() == ((width+1) * (height+1)) as usize);
         assert!(normals.len() == ((width+1) * (height+1)) as usize);
         assert!(indices.len() == (width * height * 6) as usize);
 
+        // Color overrides
+        let mut colors = vec![Color::WHITE.as_rgba_f32(); ((width+1) * (height+1)) as usize];
+        for i in 0..colors.len() {
+            if positions[i].y < -16.5 {
+                colors[i] = Color::SEA_GREEN.as_rgba_f32();
+            } else if normals[i].y < 0.82 {
+                colors[i] = Color::GRAY.as_rgba_f32();
+            } else if positions[i].y < 24. {
+                if normals[i].y > 0.98 {
+                    colors[i] = Color::LIME_GREEN.as_rgba_f32();
+                } else {
+                    colors[i] = Color::GREEN.as_rgba_f32();
+                }
+            }
+        }
+
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
         mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-        mesh.set_indices(Some(Indices::U16(indices)));
+        mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+        mesh.set_indices(Some(Indices::U32(indices)));
         let handle = meshes.add(mesh);
         TerrainPlane { mesh: handle }
-    }    
+    }
 }
